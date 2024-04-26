4cb4d0411e85dd414d53b058f4c7aacbd65248dc
Igor Dvorkin <idvorkin@gmail.com>
2024-04-26 10:43:15 -0700

    Checkpoint - can run with  modal deploy

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 #!python3
 import hyperdiv as hd
 import homepage as hp
+import typer
 import flashcards as fc
+from icecream import ic
+from modal import Image, App, forward, Function
+
+cli_app = typer.Typer()
+
+
+app = App("mathflash")  # Note: prior to April 2024, "app" was called "stub"
+app.image = Image.debian_slim().pip_install("hyperdiv;icecream".split(";"))
 
 router = hd.router()
 
@@ -20,4 +29,30 @@ def operator_page(operator, max):
     fc.operator_page(operator, max)
 
 
-hd.run(main)
+@app.function()
+def serve_on_modal():
+    import os
+
+    # setenv of HD_HOST is not working
+    port = 8887
+    os.environ["HD_HOST"] = "0.0.0.0"
+    os.environ["HD_PORT"] = str(port)
+    with forward(port) as tunnel:
+        ic(tunnel)
+        print(tunnel.url)
+        hd.run(main)
+
+
+@cli_app.command()
+def get_modal_port():
+    f = Function.lookup("math_flash", "serve_on_modal")
+    f.remote()
+
+
+@cli_app.command()
+def local():
+    hd.run(main)
+
+
+if __name__ == "__main__":
+    cli_app()
