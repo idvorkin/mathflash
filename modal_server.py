e3b344ce944d2f3f86d05bc547225d3414ffd774
Igor Dvorkin <idvorkin@gmail.com>
2024-04-27 09:14:41 -0700

    Checkpoint modal_server.py

--- modal_server.py
+++ modal_server.py
@@ -1,5 +1,4 @@
 #!python3
-import typer
 from icecream import ic
 from modal import Image, App, forward, Function, Dict, asgi_app
 from fastapi import FastAPI, Request
@@ -7,72 +6,76 @@ from fastapi.responses import RedirectResponse
 import datetime
 import requests
 import main
+import os
+import time
 
-web_app = FastAPI()
 
-
+# setup modal app state
+image = Image.debian_slim().pip_install(["hyperdiv", "icecream", "requests"])
 app = App("mathflash")  # Note: prior to April 2024, "app" was called "stub"
-MODAL_IMAGE = Image.debian_slim().pip_install(["hyperdiv","icecream", "requests"])
-app.image = MODAL_IMAGE
-
-MATHFLASH_DICT = Dict.from_name(f"{app.name}-default-dict", create_if_missing=True)
-
-# https://modal.com/docs/guide/webhooks
-@app.function(image=MODAL_IMAGE)
-@asgi_app()
-def fastapi_app():
-    return web_app
+app.image = image
+state = Dict.from_name(f"{app.name}-default-dict", create_if_missing=True)
 
 
 class KEY:
     server_url = "server_url"
     innvocation_count = "innvocation_count"
 
-@app.function(concurrency_limit=1, timeout=300)
-def serve_on_modal():
-    import os
-    ic("called at", datetime.datetime.now())
 
-    # setenv of HD_HOST is not working
-    port = 8887
-    os.environ["HD_HOST"] = "0.0.0.0"
-    os.environ["HD_PORT"] = str(port)
-    with forward(port) as tunnel:
-        MATHFLASH_DICT[KEY.server_url] = tunnel.url
-        MATHFLASH_DICT[KEY.innvocation_count]  = MATHFLASH_DICT.get(KEY.innvocation_count, 0) + 1
-        main.local_debug_server()
+# https://modal.com/docs/guide/webhooks
+web_app = FastAPI()
+
+
+@app.function(image=image)
+@asgi_app()
+def fastapi_app():
+    return web_app
+
 
 @app.function()
 @web_app.get("/{full_path:path}")
-async def read_all(request: Request, full_path: str):
-    # check if the server is running ... if not, start it
-    import time
-    server_url = MATHFLASH_DICT.get(KEY.server_url, None)
+async def get_redirect_to_server(request: Request, full_path: str):
+    server_url = state.get(KEY.server_url, None)
     is_up = is_website_up(server_url)
-    url_path = str(request.url)
-    if (not is_up):
-        print("Server is not running, starting it now ...")
-        server_url = MATHFLASH_DICT[KEY.server_url] = ""
-        f = Function.lookup(app.name, "serve_on_modal")
-        f.spawn()
-        # now busy wait for the server to start
-        while True:
-            print("Waiting for server to start ...")
-            server_url = MATHFLASH_DICT.get(KEY.server_url, None)
-            is_up = is_website_up(server_url)
-            if is_up:
-                break
-            time.sleep(1)
+    if not is_up:
+        start_server_and_wait_for_it_to_be_up()
     return RedirectResponse(f"{server_url}/{full_path}", status_code=303)
+
+
+@app.function(concurrency_limit=1, timeout=300)
+def serve_on_modal():
+    ic("called at", datetime.datetime.now())
+    port = 8887  # tell hyperdiv to bind to a port (doesn't matter which)
+    os.environ["HD_HOST"] = "0.0.0.0"
+    os.environ["HD_PORT"] = str(port)
+    with forward(port) as tunnel:
+        state[KEY.server_url] = tunnel.url  # type:ignore # tunnel.url is the public URL of the server
+        state[KEY.innvocation_count] = state.get(KEY.innvocation_count, 0) + 1  # type:ignore
+        # this call is blocking, so this never returns
+        # Run the hyperdiv app
+        main.run()
+
+
+def start_server_and_wait_for_it_to_be_up():
+    print("Server is not running, starting it now ...")
+    server_url = state[KEY.server_url] = ""  # type:ignore
+    Function.lookup(app.name, "serve_on_modal").spawn()  # type: ignore
+    # now busy wait for the server to start
+    while True:
+        print("Waiting for server to start ...")
+        server_url = state.get(KEY.server_url, None)
+        is_up = is_website_up(server_url)
+        if is_up:
+            break
+        time.sleep(1)
 
 
 def is_website_up(url):
     try:
-        response = requests.get(url)
+        response = requests.get(url, timeout=0.5)
         # If the response status code is 200, the website is considered up
         return response.status_code == 200
     except requests.exceptions.RequestException as e:
         # If there is any exception (like a connection error), the website is considered down
         print(f"Error checking website: {e}")
         return False
-
